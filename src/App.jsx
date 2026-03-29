8bb30d3d3a069714d463138112205f1eb181a2cb
ashiqurrhmn <arakash115@gmail.com>
2026-03-29 19:22:33 +0600

    setup necessary links

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h2 className='text-red-500'>Hi Guys</h2>
+    <i class="fa-solid fa-arrow-down"></i>
+    </>
+  )
+}
+
+export default App
