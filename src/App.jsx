8c383539e0a729ff5015858b9ac8a220ee419a73
ashiqurrhmn <arakash115@gmail.com>
2026-03-30 20:51:59 +0600

    tools cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,47 @@
+import { Suspense } from "react";
+import "./App.css";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import Footer2 from "./components/Footer2";
+import Tools from "./components/Tools";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Status from "./components/Status";
+import Steps from "./components/Steps";
 
-import './App.css'
-import Banner from './components/Banner'
-import Footer from './components/Footer'
-import Footer2 from './components/Footer2'
-import Navbar from './components/Navbar'
-import Pricing from './components/Pricing'
-import Status from './components/Status'
-import Steps from './components/Steps'
+const getTool = async () => {
+  const res = await fetch("/data.json");
+  return res.json();
+};
 
 function App() {
+  const toolPromise = getTool();
 
   return (
     <>
-    <Navbar />
-    <Banner />
+      <Navbar />
+      <Banner />
+      <Status />
 
+      <Suspense
+        fallback={
+          <div>
+            <span className="loading loading-spinner text-primary"></span>
+            <span className="loading loading-spinner text-secondary"></span>
+            <span className="loading loading-spinner text-accent"></span>
+            <span className="loading loading-spinner text-neutral"></span>
+          </div>
+        }
+      >
+        <Tools toolPromise={toolPromise} />
+      </Suspense>
 
-    <Status />
-    <Steps />
-    <Pricing />
-    <Footer />
-    <Footer2 />
+      <Steps />
+      <Pricing />
+      <Footer />
+      <Footer2 />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
